a59d37bf3fb2137b1c34bf18f8d563f2e813f608
stephenswetonic <sswetonc@uw.edu>
2023-06-20 14:16:45 -0700

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -166,7 +166,7 @@
     }
 </script>
 
-<div class="logotext text-3xl my-6">SentenceMixer AI</div>
+<div class="logotext text-3xl my-4">SentenceMixer AI</div>
 
 <input 
     class="file-input w-full max-w-sm mt-2"
@@ -217,20 +217,25 @@
 
 <button class="btn btn-primary btn-wide mt-4" on:click={generate}>Generate</button>
 {#if loadingGenerate}
-    <span class="loading loading-spinner loading-lg my-auto"></span>
+<div class="inline-flex h-full align-middle">
+    <span class="loading loading-spinner loading-lg"></span>
+</div>
 {/if}
 
 
-<audio controls src="" id="generatedAudio" hidden={hideAudio}>
+<audio class="my-2" controls src="" id="generatedAudio" hidden={hideAudio}>
     
 </audio>
 {#if !hideAudio}
-    <a href={audioElement?.src} download >Download</a>
+    <a href={audioElement?.src} download class="btn btn-primary inline-flex mb-4">Download</a>
 {/if}
 
 
 <!-- svelte-ignore a11y-media-has-caption -->
-<video src="" id="generatedVideo" controls hidden={hideVideo}></video>
+<video src="my-2" id="generatedVideo" controls hidden={hideVideo}></video>
+{#if !hideVideo}
+    <a href={videoElement?.src} download class="btn btn-primary inline-flex mb-4">Download</a>
+{/if}
 
 <style>
     .logotext {
