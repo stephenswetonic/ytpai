70edd357eb3a2945ae3acabb6d5e0abd48188c24
stephenswetonic <sswetonc@uw.edu>
2023-06-21 15:54:49 -0700

    about page, styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@
 
 <div class="min-h-screen bg-base-100">
     <div class="navbar bg-base-100 border-b mx-auto max-w-8xl sm:px-6 lg:px-8">
-        <div class="logotext text-3xl">SentenceMixer AI</div>
-
+        <a class="logotext btn btn-ghost text-3xl" href="/">SentenceMixer AI</a>
+        <a class="btn btn-ghost mx-2 absolute right-0" href="/about">About</a>
     </div>
     <div class="mx-auto max-w-8xl sm:px-6 lg:px-8">
         <slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<div class="m-10">
+    <div class="text-3xl font-bold tracking-light text-base-content mb-6">About</div>
+
+    <div class="my-1">SentenceMixer AI is a free tool for mashing together words from audio and video into a single clip. Think "Youtube poop" style but without the manual editing.</div>
+    <div class="my-1">Voice analysis done via Vosk AI models in the cloud. My goal is to keep this free to use and without registration.</div>
+    <div class="my-1">This service will be up until my Google Cloud free credit runs out, or I find another solution!</div>
+    <div class="my-4 text-2xl font-bold tracking-light text-base-content">Tech</div>
+    <ul class="list-disc text-base-content ml-6">
+        <li>SvelteKit</li>
+        <li>TailwindCSS</li>
+        <li>DaisyUI</li>
+        <li>Vosk</li>
+        <li>MoviePy</li>
+    </ul>
+
+    <div class="my-4 text-2xl font-bold tracking-light text-base-content">By Stephen Swetonic</div>
+
+    <a class="btn rounded mr-2 font-medium hover:cursor-pointer" href="https://github.com/stephenswetonic">Github</a>
+
+    <a class="btn rounded mr-2 font-medium hover:cursor-pointer" href="https://www.linkedin.com/in/stephen-swetonic-095a04133/">LinkedIn</a>
+</div>
