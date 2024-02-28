4dff08209494e314c673d7324dd36586b23e3db9
stephenswetonic <sswetonc@uw.edu>
2024-02-28 10:58:08 -0800

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,11 @@
     <div class="my-1">SentenceMixer AI is a free tool for mashing together words from audio and video into a single clip. Think "Youtube poop" style but without the manual editing.</div>
     <div>Drag on drop doesn't really work on mobile. I may create a different UI for mobile.</div>
     <div class="my-1">Voice analysis done via Vosk AI models in the cloud. My goal is to keep this free to use and without registration.</div>
-    <div class="my-1">This service will be up until my Google Cloud free credit runs out, or I find another solution!</div>
+    <div class="my-1">After a long hiatus, the site is back with a new AWS Lambda backend.</div>
+
+    <div class="my-4 text-2xl font-bold tracking-light text-base-content">Suggestions?</div>
+    <div class="my-1">Please raise an issue on <a class="text-blue-500 hover:text-blue-700 font-semibold" href="https://github.com/stephenswetonic/ytpai">Github.</a></div>
+
     <div class="my-4 text-2xl font-bold tracking-light text-base-content">Tech</div>
     <ul class="list-disc text-base-content ml-6">
         <li>SvelteKit</li>
@@ -12,6 +16,7 @@
         <li>DaisyUI</li>
         <li>Vosk</li>
         <li>MoviePy</li>
+        <li>AWS Lambda</li>
     </ul>
     <div class="my-4 text-2xl font-bold tracking-light text-base-content">Future Work</div>
     <ul class="list-disc text-base-content ml-6">
