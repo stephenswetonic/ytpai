3f033dc34d01394f1a9b4f3a59db7ac960bc915d
stephenswetonic <sswetonc@uw.edu>
2024-03-04 13:42:12 -0800

    new alert message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -258,10 +258,10 @@
     }
 </script>
 
-<div role="alert" class="alert alert-warning">
-    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-    <span>Undergoing server updates. If loading fails, try again later.</span>
-</div>
+<div role="alert" class="alert">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <span>The more accurate "Big Model" for English is still under development. Files that take longer than 30 seconds to process will time out for now.</span>
+  </div>
 
 <input
     class="file-input w-full max-w-sm mt-2"
@@ -423,8 +423,7 @@
         <span class="loading loading-spinner loading-lg"></span>
     </div>
 {/if}
-<div class="text-sm">Supports audio (.wav) or video (.mp4) under 100MB</div>
-<div class="text-sm">Long videos may time out!</div>
+<div class="text-sm">Supports audio (.wav) or video (.mp4)</div>
 
 <ul class="text-sm">
     <li>
