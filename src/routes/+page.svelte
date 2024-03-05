72d4c06b786d5cba54572beb02d45d5f75c9f780
stephenswetonic <sswetonc@uw.edu>
2024-03-04 16:54:44 -0800

    Alert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -258,6 +258,11 @@
     }
 </script>
 
+<div role="alert" class="alert alert-warning">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+    <span>Undergoing server work. Uploads will likely time out.</span>
+  </div>
+
 <div role="alert" class="alert">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
     <span>The more accurate "Big Model" for English is still under development. Files that take longer than 30 seconds to process will time out for now.</span>
