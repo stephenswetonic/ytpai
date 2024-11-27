24c0747200f7e2de4cbc1f0167d77caa408be4ed
stephenswetonic <sswetonc@uw.edu>
2024-11-26 18:15:35 -0800

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,10 +2,10 @@
     <div class="text-3xl font-bold tracking-light text-base-content mb-6">About</div>
 
     <div class="my-1">SentenceMixer AI is a free tool for mashing together words from audio and video into a single clip. Think Youtube poop/SFM style but without the manual editing.</div>
-    <div>Drag on drop doesn't really work on mobile. I may create a different UI for mobile.</div>
-    <div class="my-1">Voice analysis done via Open AI Whisper in the cloud. My goal is to keep this free to use and without registration.</div>
+    <div>All files are deleted from the server daily.</div>
+    <div class="my-1">Voice analysis done via WhisperX on my local server. Clip generation done on AWS Lambda. My goal is to keep this free to use and without registration.</div>
 
-    <div class="my-4 text-2xl font-bold tracking-light text-base-content">Suggestions?</div>
+    <div class="my-4 text-2xl font-bold tracking-light text-base-content">Suggestions or issues?</div>
     <div class="my-1">Please raise an issue on <a class="text-blue-500 hover:text-blue-700 font-semibold" href="https://github.com/stephenswetonic/ytpai">Github.</a></div>
 
     <div class="my-4 text-2xl font-bold tracking-light text-base-content">Tech</div>
@@ -13,7 +13,7 @@
         <li>SvelteKit</li>
         <li>TailwindCSS</li>
         <li>DaisyUI</li>
-        <li>Open AI Whisper</li>
+        <li>WhisperX</li>
         <li>FFMPEG Web Assembly</li>
         <li>AWS Lambda</li>
     </ul>
